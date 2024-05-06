c2522fa27351d2805761f09a0d212d6a60db190d
mohamedfawas <fawastmh@gmail.com>
2024-05-06 09:55:48 +0530

    power of 3

---
+++ day22_6May/power_of_three326.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func isPowerOfThree(n int) bool {
+	if n == 1 {
+		return true
+	}
+	if n%3 != 0 || n <= 0 {
+		return false
+	}
+	return isPowerOfThree(n / 3)
+}
+
+func main() {
+	fmt.Println()
+}
